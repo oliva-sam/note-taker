0a517018dcb95ab5f64bc7dcf884358ca4a0fc55
Melody Monarch <oliva.sam.so@gmail.com>
2021-01-09 16:15:02 -0600

    connect the js routes and rearranged the callback

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 var PORT = 8000;
 
 var htmlRoutes = require ("./routes/htmlroutes.js");
-
+var apiRoutes = require ("./routes/apiroutes.js");
 // default code 
 app.use(express.urlencoded({
     extended: true,
@@ -14,7 +14,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+app.use(apiRoutes);
 app.use(htmlRoutes);
+
 
 app.listen(PORT, function() {
     console.log(`App listening on PORT : ${PORT}`)
